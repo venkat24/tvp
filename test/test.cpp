37504f4712d8668784caf0410d2fb38b2ddd8efc
venkat24 <venkatramansrikanth@outlook.com>
2018-10-20 06:08:53 +0530

    Add Google Test framework

---
+++ test/test.cpp
@@ -0,0 +1,10 @@
+#include "gtest/gtest.h"
+
+using namespace std;
+
+TEST(Test, TestingTest) { EXPECT_EQ(1+1, 2); }
+
+int main(int argc, char *argv[]) {
+	testing::InitGoogleTest(&argc, argv);
+	return RUN_ALL_TESTS();
+}
