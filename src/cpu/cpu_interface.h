be994d7957f94b9cb65aeba4fc61d7124ffda87d
venkat24 <venkatramansrikanth@outlook.com>
2018-10-20 06:48:45 +0530

    Add Interface for CPU
    - Declare some stub functions for a CPU tick and opcode execute
    - Add utils.h to put more CPU stuff in later. Contains some aliases for
    now

---
+++ src/cpu/cpu_interface.h
@@ -0,0 +1,38 @@
+/**
+ * @file cpu_interface.h
+ * Declares the interface for a CPU
+ */
+
+#include <cstdint>
+#include "utils.h"
+
+#ifndef CPU_CPU_INTERFACE_H
+#define CPU_CPU_INTERFACE_H
+
+namespace cpu {
+
+/**
+ * Define the interface for changing the CPU state
+ * The only allowed external actions are to feed in a new opcode,
+ * and increment the clock with a tick
+ */
+class CPUInterface {
+  public:
+    /**
+     * Increments the clock by 1
+     */
+    virtual void tick() = 0;
+
+    /**
+     * Run an instruction with the specified opcode at the specified
+     * location of the program counter
+     * 
+     * @param opcode The opcode
+     * @param pc The Program Counter Value
+     */
+    virtual void execute(OpCode opcode, ProgramCounter pc) = 0;
+};
+
+} // namespace cpu
+
+#endif
---
+++ src/cpu/utils.h
@@ -0,0 +1,14 @@
+/**
+ * @file utils.h
+ * Declares helpers and utils for the CPU class
+ */
+
+#include <cstdint>
+
+#ifndef CPU_UTILS_H
+#define CPU_UTILS_H
+
+using OpCode = uint8_t;
+using ProgramCounter = uint16_t;
+
+#endif
