6a1b81f480cd87e50bc2d0fef73b03d66f9a56ac
venkat24 <venkatramansrikanth@outlook.com>
2018-10-30 06:01:48 +0530

    Add opcodes.cpp
    - opcodes.cpp will contain definitions for the helper methods
    corresponding to each CPU opcode

---
+++ src/cpu/src/opcodes.cpp
@@ -0,0 +1,11 @@
+/**
+ * @file opcodes.cpp
+ * Contains implementations for all the CPU base opcode functions
+ */
+
+#include "cpu/cpu.h"
+#include "cpu/register/register.h"
+
+namespace cpu {
+
+} // namespace cpu
