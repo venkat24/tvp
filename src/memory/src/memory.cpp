e4a7d3ea19980cb1208950fe60cda449ff513f8c
venkat24 <venkatramansrikanth@outlook.com>
2018-11-09 07:13:37 +0530

    Add memory mapping implementation
    - Maps :
        - Memory blocks (VRAM, WRAM, HRAM, etc.)
        - CPU Registers (Interrupt Enable, Interrupt Flag)
        - GPU Registers (LCDC, STAT, LYC, LY, etc.)
    
    - Placeholders for Audio, Cartridge, etc.

--- src/memory/src/memory.cpp
+++ src/memory/src/memory.cpp
@@ -4,21 +4,271 @@
  */
 
 #include "memory/memory.h"
+#include "util/helpers.h"
+#include "util/log.h"
 
 namespace memory {
 
 Memory::Memory() : memory(std::array<uint8_t, 0x10000>()) {}
 
+bool address_in_range(Address addr, Address start, Address end) {
+	if ((addr >= start && addr <= end) || (addr >= end && addr <= start)) {
+		return true;
+	}
+	return false;
+}
+
 uint8_t Memory::read(Address address) const {
-	// Naive implementation
-	// TODO: Switch case this based on device memory mapping
+	// Interrupt Enable Register
+	if (address == 0xFFFF) {
+		return cpu->get_interrupt_enable()->get();
+	}
+
+	// High RAM
+	if (address_in_range(address, 0xFFFE, 0xFF80)) {
+		return memory[address];
+	}
+
+	// GPU Registers
+	if (address_in_range(address, 0xFF4B, 0xFF40)) {
+		auto register_offset = static_cast<uint16_t>(address - 0xFF40);
+		switch (register_offset) {
+		case 0x0:
+			return gpu->get_lcdc()->get();
+		case 0x1:
+			return gpu->get_stat()->get();
+		case 0x2:
+			return gpu->get_scy()->get();
+		case 0x3:
+			return gpu->get_scx()->get();
+		case 0x4:
+			return gpu->get_ly()->get();
+		case 0x5:
+			return gpu->get_lyc()->get();
+		case 0x6:
+			Log::warn("Cannot read from DMA register");
+			return 0xFFFF; // DMA is non-readable
+		case 0x7:
+			return gpu->get_bgp()->get();
+		case 0x8:
+			return gpu->get_obp0()->get();
+		case 0x9:
+			return gpu->get_obp1()->get();
+		case 0xA:
+			return gpu->get_wy()->get();
+		case 0xB:
+			return gpu->get_wx()->get();
+		}
+	}
+
+	// Sound Controller Registers
+	if (address_in_range(address, 0xFF26, 0xFF10)) {
+		// TODO: Sound Controller
+		Log::warn("Attempt to read from sound register" + num_to_hex(address));
+		return 0xFFFF;
+	}
+
+	// Interrupt Flag register
+	if (address == 0xFF0F) {
+		return cpu->get_interrupt_flag()->get();
+	}
+
+	// Restricted memory
+	if (address_in_range(address, 0xFEFF, 0xFEA0)) {
+		// Invalid Memory addresses!
+		Log::fatal("Tried to access location " + num_to_hex(address));
+	}
+
+	// OAM
+	if (address_in_range(address, 0xFE9F, 0xFE00)) {
+		return memory[address];
+	}
+
+	// Echo RAM, returns copy of RAM
+	if (address_in_range(address, 0xFDFF, 0xE000)) {
+		Log::warn("Reading from " + num_to_hex(address) + " which is Echo RAM");
+		return memory[address - 0x2000];
+	}
+
+	// Main Work RAM
+	if (address_in_range(address, 0xDFFF, 0xC000)) {
+		return memory[address];
+	}
+
+	// Cartridge RAM
+	if (address_in_range(address, 0xBFFF, 0xA000)) {
+		Log::warn("Tried to access Cartridge RAM from " + num_to_hex(address));
+		return 0xFFFF;
+		// TODO: Return Cartridge RAM if available
+	}
+
+	// BG Data Maps
+	if (address_in_range(address, 0x9FFF, 0x9800)) {
+		return memory[address];
+	}
+
+	// VRAM
+	if (address_in_range(address, 0x97FF, 0x8000)) {
+		return memory[address];
+	}
+
+	// Cartridge Data
+	if (address_in_range(address, 0x7FFF, 0x0100)) {
+		Log::warn("Tried to access Cartridge Data from " + num_to_hex(address));
+		return 0xFFFF;
+		// TODO: Return Cartridge Data
+	}
+
+	// Interrupt Vectors and Boot Rom
+	if (address_in_range(address, 0x00FF, 0x0000)) {
+		// If 0xFF50 is set, Boot ROM is disabled
+		if (memory[0xFF50]) {
+			// TODO: Return Cartridge Data
+			Log::warn("Tried to access Cartridge Data from " +
+			          num_to_hex(address));
+			return 0xFFFF;
+		} else {
+			return boot[address];
+		}
+	}
+
+	Log::error("Default for location " + num_to_hex(address) + " returned!");
+
 	return memory[address];
 }
 
 void Memory::write(Address address, uint8_t data) {
-	// Naive implementation
-	// TODO: Switch case this based on device memory mapping
-	memory[address] = data;
+	// Interrupt Enable Register
+	if (address == 0xFFFF) {
+		cpu->get_interrupt_enable()->set(data);
+		return;
+	}
+
+	// High RAM
+	if (address_in_range(address, 0xFFFE, 0xFF80)) {
+		memory[address] = data;
+		return;
+	}
+
+	/// GPU Registers
+	if (address_in_range(address, 0xFF4B, 0xFF40)) {
+		auto register_offset = static_cast<uint16_t>(address - 0xFF40);
+		switch (register_offset) {
+		case 0x0:
+			gpu->get_lcdc()->set(data);
+			return;
+		case 0x1:
+			gpu->get_stat()->set(data);
+			return;
+		case 0x2:
+			gpu->get_scy()->set(data);
+			return;
+		case 0x3:
+			gpu->get_scx()->set(data);
+			return;
+		case 0x4:
+			Log::error("Cannot write to LY register location");
+			return;
+		case 0x5:
+			gpu->get_lyc()->set(data);
+			return;
+		case 0x6:
+			gpu->get_dma()->set(data);
+			return;
+		case 0x7:
+			gpu->get_bgp()->set(data);
+			return;
+		case 0x8:
+			gpu->get_obp0()->set(data);
+			return;
+		case 0x9:
+			gpu->get_obp1()->set(data);
+			return;
+		case 0xA:
+			gpu->get_wy()->set(data);
+			return;
+		case 0xB:
+			gpu->get_wx()->set(data);
+			return;
+		}
+	}
+
+	// Sound Controller Registers
+	if (address_in_range(address, 0xFF26, 0xFF10)) {
+		// TODO: Sound Controller
+		Log::warn("Attempt to write to sound register " + num_to_hex(address));
+		return;
+	}
+
+	// Interrupt Flag register
+	if (address == 0xFF0F) {
+		cpu->get_interrupt_flag()->set(data);
+		return;
+	}
+
+	// Restricted memory
+	if (address_in_range(address, 0xFEFF, 0xFEA0)) {
+		// Invalid Memory addresses!
+		Log::fatal("Tried to write to location " + num_to_hex(address));
+	}
+
+	// OAM
+	if (address_in_range(address, 0xFE9F, 0xFE00)) {
+		memory[address] = data;
+		return;
+	}
+
+	// Echo RAM, returns copy of RAM
+	if (address_in_range(address, 0xFDFF, 0xE000)) {
+		Log::warn("Writing to " + num_to_hex(address) + " which is Echo RAM");
+		memory[address - 0x2000] = data;
+		return;
+	}
+
+	// Main Work RAM
+	if (address_in_range(address, 0xDFFF, 0xC000)) {
+		memory[address] = data;
+		return;
+	}
+
+	// Cartridge RAM
+	if (address_in_range(address, 0xBFFF, 0xA000)) {
+		// TODO: Return Cartridge RAM if available
+		Log::warn("Tried to write to Cart RAM from " + num_to_hex(address));
+		return;
+	}
+
+	// BG Data Maps
+	if (address_in_range(address, 0x9FFF, 0x9800)) {
+		memory[address] = data;
+		return;
+	}
+
+	// VRAM
+	if (address_in_range(address, 0x97FF, 0x8000)) {
+		memory[address] = data;
+		return;
+	}
+
+	// Cartridge Data
+	if (address_in_range(address, 0x7FFF, 0x0100)) {
+		// TODO: Return Cartridge Data
+		Log::warn("Tried to write to Cart Data from " + num_to_hex(address));
+		return;
+	}
+
+	// Interrupt Vectors
+	if (address_in_range(address, 0x00FF, 0x0000)) {
+		// TODO: Return Cartridge Data
+		Log::warn("Tried to write to Cart RAM from " + num_to_hex(address));
+		return;
+	}
+
+	Log::fatal("Attempt to write to location " + num_to_hex(address));
 }
+
+void Memory::set_cpu(cpu::CPUInterface *p_cpu) { cpu = p_cpu; }
+
+void Memory::set_gpu(gpu::GPUInterface *p_gpu) { gpu = p_gpu; }
 
 } // namespace memory
