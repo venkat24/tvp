9b9f3c28bc575fece994b430e9014674266acd2c
venkat24 <venkatramansrikanth@outlook.com>
2018-11-09 06:50:13 +0530

    Add Headers for the GPU class
    - Declares the GameBoy hardware's PPU functionality
    - Declares all the internal registers, and some other state variables
    and helper functions
    - Adds the Utils header for various constants relating to the GPU

---
+++ src/gpu/include/gpu/gpu.h
@@ -0,0 +1,207 @@
+/**
+ * @file gpu.h
+ * Declares the GPU Class
+ */
+
+#include "cpu/cpu_interface.h"
+#include "cpu/register/register_interface.h"
+#include "cpu/utils.h"
+#include "gpu/gpu_interface.h"
+#include "gpu/utils.h"
+#include "memory/memory_interface.h"
+
+#include <cstdint>
+#include <memory>
+
+#ifndef GPU_INCLUDE_GPU_GPU_H
+#define GPU_INCLUDE_GPU_GPU_H
+
+namespace gpu {
+
+/**
+ * The GPU class, which controls pixel display to the screen
+ */
+class GPU : public GPUInterface {
+	using Reg = cpu::RegisterInterface;
+
+	/**
+	 * LCD Control Register @FF40
+	 * Contains bits controlling various display states
+	 */
+	std::unique_ptr<Reg> lcdc;
+
+	/**
+	 * LCD Status Register @FF41
+	 * Contains bits controlling various rendering modes
+	 */
+	std::unique_ptr<Reg> stat;
+
+	/**
+	 * LCD Scroll Y @FF42
+	 * Specifies the Y position in the 256x256 pixels BG map (32x32 tiles)
+	 * which is to be displayed at the upper/left LCD display position.
+	 */
+	std::unique_ptr<Reg> scy;
+
+	/**
+	 * LCD Scroll Y @FF43
+	 * Specifies the X position in the 256x256 pixels BG map (32x32 tiles)
+	 * which is to be displayed at the upper/left LCD display position.
+	 */
+	std::unique_ptr<Reg> scx;
+
+	/**
+	 * LCDC Y-Coordinate @FF44
+	 * The LY indicates the vertical line to which the present data is
+	 * transferred to the LCD Driver.
+	 */
+	std::unique_ptr<Reg> ly;
+
+	/**
+	 * LYC LC-Compare @FF45
+	 * This register is constantly compared with the LCDC register and the
+	 * corresponding bits in STAT are set depending on this
+	 */
+	std::unique_ptr<Reg> lyc;
+
+	/**
+	 * Window Y Position @FF4A
+	 * Holds the Y position of the overlay window
+	 */
+	std::unique_ptr<Reg> wy;
+
+	/**
+	 * Window X Position @FF4B
+	 * Holds the X position of the overlay window, subtracted by 7 (wx - 7)
+	 */
+	std::unique_ptr<Reg> wx;
+
+	/**
+	 * BG Palette Data @FF47
+	 * This register assigns gray shades to the color numbers of the BG and
+	 * Window tiles.
+	 *   Bit 7-6 - Shade for Color Number 3
+	 *   Bit 5-4 - Shade for Color Number 2
+	 *   Bit 3-2 - Shade for Color Number 1
+	 *   Bit 1-0 - Shade for Color Number 0
+	 * The four possible gray shades are:
+	 *   00  White
+	 *   01  Light gray
+	 *   10  Dark gray
+	 *   11  Black
+	 */
+	std::unique_ptr<Reg> bgp;
+
+	/**
+	 * Object Palette 0 Data @FF48
+	 * This register assigns gray shades for sprite palette 0. It works exactly
+	 * as BGP (FF47), except that the lower two bits aren't used because sprite
+	 * data 00 is transparent.
+	 */
+	std::unique_ptr<Reg> obp0;
+
+	/**
+	 * Object Palette 1 Data @FF49
+	 * This register assigns gray shades for sprite palette 1. It works exactly
+	 * as BGP (FF47), except that the lower two bits aren't used because sprite
+	 * data 00 is transparent.
+	 */
+	std::unique_ptr<Reg> obp1;
+
+	/**
+	 * DMA Transfer @FF46
+	 * Writing to this register launches a DMA transfer from ROM or RAM to OAM
+	 * memory (sprite attribute table). The written value specifies the transfer
+	 * source address divided by 100h
+	 */
+	std::unique_ptr<Reg> dma;
+
+	/**
+	 * Memory instance, for performing reads and writes to main memory
+	 */
+	memory::MemoryInterface *memory;
+
+	/**
+	 * CPU instance, for interrupting the CPU
+	 */
+	cpu::CPUInterface *cpu;
+
+	/**
+	 * The current mode that the GPU is in
+	 */
+	GPUMode mode;
+
+	/**
+	 * Count the number of cycles elapsed in the current mode of the GPU. This
+	 * is used to check for transitions to the next mode.
+	 *
+	 * TODO: Note that the quantum of video output in TVP is one mode of the
+	 * scanline, not the pixel. Currently there is no Pixel FIFO implementation,
+	 * so draw actions are performed only once per scanline, and not per pixel
+	 * like it would in the real GameBoy hardware.
+	 */
+	cpu::ClockCycles current_cycles;
+
+	/**
+	 * Video Buffer
+	 * This is a 2D array that contains the complete contents of the current
+	 * frame to be drawn. This will be sent to the Video driver every frame to
+	 * be painted on to the screen.
+	 */
+	VideoBuffer v_buffer;
+
+	/**
+	 * Set the mode and the LCD Status register bits to match
+	 */
+	void change_mode(GPUMode mode);
+
+	/**
+	 * Fire a particular interrupt. Set the corresponding bit in the CPU
+	 * interrupt flag register
+	 */
+	void fire_interrupt(cpu::Interrupt interrupt);
+
+	/**
+	 * Write the current scanline of pixels into the video buffer
+	 */
+	void write_line();
+
+	/**
+	 * Write all sprites for the current frame into the video buffer
+	 */
+	void write_sprites();
+
+  public:
+	GPU(std::unique_ptr<Reg> lcdc, std::unique_ptr<Reg> stat,
+	    std::unique_ptr<Reg> scy, std::unique_ptr<Reg> scx,
+	    std::unique_ptr<Reg> ly, std::unique_ptr<Reg> lyc,
+	    std::unique_ptr<Reg> wy, std::unique_ptr<Reg> wx,
+	    std::unique_ptr<Reg> bgp, std::unique_ptr<Reg> obp0,
+	    std::unique_ptr<Reg> obp1, std::unique_ptr<Reg> dma,
+	    memory::MemoryInterface *memory, cpu::CPUInterface *cpu);
+
+	/**
+	 * @see GPUInterface#tick
+	 */
+	void tick(cpu::ClockCycles cycles) override;
+
+	/// Getters for Registers
+	/// Simply return a pointer so that Memory can manipulate these values with
+	/// easily, as each register corresponds to a memory location
+	Reg *get_lcdc() override;
+	Reg *get_stat() override;
+	Reg *get_scy() override;
+	Reg *get_scx() override;
+	Reg *get_ly() override;
+	Reg *get_lyc() override;
+	Reg *get_wy() override;
+	Reg *get_wx() override;
+	Reg *get_bgp() override;
+	Reg *get_obp0() override;
+	Reg *get_obp1() override;
+	Reg *get_dma() override;
+};
+
+} // namespace gpu
+
+#endif
---
+++ src/gpu/include/gpu/utils.h
@@ -0,0 +1,107 @@
+/**
+ * @file utils.h
+ * Declares helpers and utils for the GPU class
+ */
+
+#include "cpu/utils.h"
+#include "memory/utils.h"
+
+#include <array>
+#include <cstdint>
+
+#ifndef GPU_INCLUDE_GPU_UTILS_H
+#define GPU_INCLUDE_GPU_UTILS_H
+
+namespace gpu {
+
+/**
+ * Represents a single pixel on the LCD display. It can be only one of 4 colors
+ */
+enum class Pixel : uint8_t { ZERO = 0, ONE = 1, TWO = 2, THREE = 3 };
+
+/**
+ * Constants representing the LCD screen width and height, in pixels
+ */
+const uint8_t SCREEN_WIDTH = 160;
+const uint8_t SCREEN_HEIGHT = 144;
+constexpr uint PIXEL_COUNT = SCREEN_WIDTH * SCREEN_HEIGHT;
+
+/**
+ * Type alias to represent a single frame of video
+ */
+using VideoBuffer = std::array<Pixel, PIXEL_COUNT>;
+
+/**
+ * Modes of the GPU frame cycle
+ */
+enum class GPUMode { OAM, VRAM, HBLANK, VBLANK };
+
+/**
+ * Constants for number of clock Cycles in each stage of the GPU frame cycle
+ * Steps :
+ *      Process for each scanline: OAM -> VRAM -> HBLANK
+ *      Process for each frame:    (All Scanlines) -> VBLANK
+ */
+const cpu::ClockCycles CLOCKS_OAM = 80;
+const cpu::ClockCycles CLOCKS_VRAM = 172;
+const cpu::ClockCycles CLOCKS_HBLANK = 204;
+
+constexpr cpu::ClockCycles CLOCKS_SCANLINE =
+    CLOCKS_OAM + CLOCKS_VRAM + CLOCKS_HBLANK;
+
+const cpu::ClockCycles CLOCKS_VBLANK = CLOCKS_SCANLINE * 10;
+constexpr cpu::ClockCycles CLOCKS_FRAME =
+    (SCREEN_HEIGHT * CLOCKS_SCANLINE) + CLOCKS_VBLANK;
+
+namespace lcdc_flag {
+/**
+ * LCD Control Register Bit Numbers
+ * Bit 7 - LCD Display Enable             (0=Off, 1=On)
+ * Bit 6 - Window Tile Map Display Select (0=9800-9BFF, 1=9C00-9FFF)
+ * Bit 5 - Window Display Enable          (0=Off, 1=On)
+ * Bit 4 - BG & Window Tile Data Select   (0=8800-97FF, 1=8000-8FFF)
+ * Bit 3 - BG Tile Map Display Select     (0=9800-9BFF, 1=9C00-9FFF)
+ * Bit 2 - OBJ (Sprite) Size              (0=8x8, 1=8x16)
+ * Bit 1 - OBJ (Sprite) Display Enable    (0=Off, 1=On)
+ * Bit 0 - BG Display                     (0=Off, 1=On)
+ */
+enum LCDControlFlagBits : uint8_t {
+	DISPLAY_ENABLE = 7,
+	WINDOW_TILE_SELECT = 6,
+	WINDOW_DISPLAY_ENABLE = 5,
+	BG_TILE_DATA_SELECT = 4,
+	BG_TILE_MAP_DISPLAY_SELECT = 3,
+	SPRITE_SIZE = 2,
+	SPRITE_DISPLAY_ENABLE = 1,
+	BG_DISPLAY = 0
+};
+} // namespace lcdc_flag
+
+namespace stat_flag {
+/**
+ * LCD Status Register Bit Numbers
+ * Bit 6 - LYC=LY Coincidence Interrupt (1=Enable) (Read/Write)
+ * Bit 5 - Mode 2 OAM Interrupt         (1=Enable) (Read/Write)
+ * Bit 4 - Mode 1 V-Blank Interrupt     (1=Enable) (Read/Write)
+ * Bit 3 - Mode 0 H-Blank Interrupt     (1=Enable) (Read/Write)
+ * Bit 2 - Coincidence Flag  (0:LYC<>LY, 1:LYC=LY) (Read Only)
+ * Bit 1-0 - Mode Flag                  (Mode 0-3) (Read Only)
+ *           0: During H-Blank
+ *           1: During V-Blank
+ *           2: During Searching OAM-RAM
+ *           3: During Transfering Data to LCD Driver
+ */
+enum LCDStatFlagBits : uint8_t {
+	LYC_COINCIDENCE_INTERRUPT_ENABLE = 6,
+	OAM_INTERRUPT_ENABLE = 5,
+	VBLANK_INTERRUPT_ENABLE = 4,
+	HBLANK_INTERRUPT_ENABLE = 3,
+	LYC_COINCIDENCE = 2,
+	MODE_HIGH_BIT = 1,
+	MODE_LOW_BIT = 0
+};
+} // namespace stat_flag
+
+} // namespace gpu
+
+#endif
