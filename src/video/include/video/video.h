1cb76622782df8efd063aefd68a8ce74a085d7ec
venkat24 <venkatramansrikanth@outlook.com>
2018-12-14 20:18:41 +0530

    Add GUI for Video module
    - Add SFML Libs
    - Add FindSFML.cmake script
    - Draw Pixelbuffer onto screen with experimental colors
    - Handle window events for key presses

--- src/video/include/video/video.h
+++ src/video/include/video/video.h
@@ -5,19 +5,53 @@
 #ifndef VIDEO_INCLUDE_VIDEO_VIDEO_H
 #define VIDEO_INCLUDE_VIDEO_VIDEO_H
 
+#include "controller/controller_interface.h"
 #include "gpu/utils.h"
 #include "video/video_interface.h"
 
+#include <SFML/Graphics.hpp>
+#include <SFML/Window.hpp>
 #include <array>
 
 namespace video {
 
 class Video : public VideoInterface {
+  private:
+	/**
+	 * Window context that the application will be rendered in
+	 */
+	std::unique_ptr<sf::RenderWindow> window;
+
+	/**
+	 * Window texture, that serves as an in-memory buffer of the output pixels
+	 */
+	std::unique_ptr<sf::Texture> window_texture;
+
+	/**
+	 * Window image, that the texture is loaded
+	 */
+	std::unique_ptr<sf::Image> window_image;
+
+	/**
+	 * Window sprite that the texture is loaded to
+	 */
+	std::unique_ptr<sf::Sprite> window_sprite;
+
+	/**
+	 * Pointer to controller instance
+	 */
+	controller::ControllerInterface *controller;
+
+	/**
+	 * Processes all external events like key presses
+	 */
+	void event_handler();
+
   public:
 	/**
 	 * Constructor
 	 */
-	Video();
+	Video(controller::ControllerInterface *controller);
 
 	/**
 	 * Print the contents of the buffer to the terminal
--- src/video/src/video.cpp
+++ src/video/src/video.cpp
@@ -5,37 +5,120 @@
 
 #include "video/video.h"
 #include "gpu/utils.h"
+#include "util/helpers.h"
+#include "util/log.h"
+
+#include <SFML/Graphics.hpp>
+#include <SFML/Window.hpp>
 
 #include <iostream>
-using namespace std;
+#include <optional>
+
 using namespace gpu;
+using namespace controller;
+
+const auto MULT = 3;
 
 namespace video {
 
-Video::Video() {}
+Video::Video(ControllerInterface *controller)
+    : window(std::make_unique<sf::RenderWindow>(
+          sf::VideoMode(SCREEN_WIDTH * MULT, SCREEN_HEIGHT * MULT),
+          "Welcome to TVP")),
+      window_texture(std::make_unique<sf::Texture>()),
+      window_image(std::make_unique<sf::Image>()),
+      window_sprite(std::make_unique<sf::Sprite>()), controller(controller) {
 
-void Video::paint(VideoBuffer &v_buffer) {
-	for (int i = 0; i < v_buffer.size(); ++i) {
-		switch (v_buffer[i]) {
-		case Pixel::ZERO:
-			cout << "\033[1;37m███\033[0m";
-			break;
-		case Pixel::ONE:
-			cout << " " << static_cast<int>(v_buffer[i]) << " ";
-			break;
-		case Pixel::TWO:
-			cout << "\033[1;30m███\033[0m";
-			break;
-		case Pixel::THREE:
-			cout << " " << static_cast<int>(v_buffer[i]) << " ";
-			break;
+	// Set some window properties
+	window->setVerticalSyncEnabled(true);
+	window->setFramerateLimit(59.97f);
+
+	// Set the image size
+	window_image->create(SCREEN_WIDTH * MULT, SCREEN_HEIGHT * MULT);
+}
+
+std::optional<Button> get_button_from_code(int64_t code) {
+	switch (code) {
+	case sf::Keyboard::W:
+		return Button::UP;
+	case sf::Keyboard::S:
+		return Button::DOWN;
+	case sf::Keyboard::A:
+		return Button::LEFT;
+	case sf::Keyboard::D:
+		return Button::RIGHT;
+	case sf::Keyboard::K:
+		return Button::A;
+	case sf::Keyboard::L:
+		return Button::B;
+	case sf::Keyboard::BackSpace:
+		return Button::SELECT;
+	case sf::Keyboard::Return:
+		return Button::START;
+	default:
+		return {};
+	}
+}
+
+void Video::event_handler() {
+	sf::Event event;
+	while (window->pollEvent(event)) {
+		// Handle key presses
+		if (event.type == sf::Event::KeyPressed) {
+			if (auto button = get_button_from_code(event.key.code)) {
+				controller->press_button(*button);
+			}
 		}
 
-		if (i % SCREEN_WIDTH == (SCREEN_WIDTH - 1)) {
-			cout << endl;
+		if (event.type == sf::Event::KeyReleased) {
+			if (auto button = get_button_from_code(event.key.code)) {
+				controller->release_button(*button);
+			}
+		}
+
+		if (event.type == sf::Event::Closed) {
+			window->close();
 		}
 	}
-	cout << endl << endl;
+}
+
+void Video::paint(VideoBuffer &v_buffer) {
+	// Handle window events
+	event_handler();
+
+	// Create pixel buffer from input
+	for (int i = 0; i < PIXEL_COUNT; ++i) {
+		auto pixel = v_buffer[i];
+		sf::Color color;
+		switch (pixel) {
+		case Pixel::ZERO:
+			color = sf::Color::White;
+			break;
+		case Pixel::ONE:
+			color = sf::Color::Red;
+			break;
+		case Pixel::TWO:
+			color = sf::Color::Black;
+			break;
+		case Pixel::THREE:
+			color = sf::Color::Black;
+			break;
+		}
+		for (int k = 0; k < MULT; k++) {
+			for (int l = 0; l < MULT; l++) {
+				auto a = (i % SCREEN_WIDTH) * MULT + k;
+				auto b = (i / SCREEN_WIDTH) * MULT + l;
+				window_image->setPixel(a, b, color);
+			}
+		}
+	}
+
+	// Draw window
+	window->clear();
+	window_texture->loadFromImage(*(window_image));
+	window_sprite->setTexture(*(window_texture), true);
+	window->draw(*(window_sprite));
+	window->display();
 }
 
 } // namespace video
